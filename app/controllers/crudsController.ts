f34aa137d679b963bff588fb8e669b01deab0e6c
AH-Raihan <dev.ahraihan@gmail.com>
2025-09-13 19:46:52 +0600

    'update

--- app/controllers/crudsController.ts
+++ app/controllers/crudsController.ts
@@ -1,34 +1,31 @@
 import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
-import { request } from 'http'
 
 export default class CrudsController {
     public async index({ view } : HttpContext){
-        
-
         const users = await User.all()
         return view.render('pages/admin/dashboard',{users})
     }
-    public async create(){
+    public async create({ response }: HttpContext){
         const user = new User()
         const today = new Date()
         user.fullName = "Raihanssss"
         user.email=`${today.getTime()}@gmail.com`
         user.password = 'hh'
-        user.save()
+        await user.save()
 
-        return Response.json(user)
+        return response.json(user)
     }
     async edit({params,view} : HttpContext){
         const user = await User.findOrFail(params.id)
         return view.render('pages/admin/edit',{user})
     }
-    async update({request} : HttpContext){
+    async update({request, response} : HttpContext){
         const user = await User.findOrFail(request.input('id'))
         user.fullName = request.input('fullname')
         user.email= request.input('email')
         user.password = 'hhd'
         await user.save()
-        return 'successfully updated'+request.input('id')
+        return response.redirect().toRoute('list')
     }
 }
--- build/app/controllers/crudsController.js
+++ build/app/controllers/crudsController.js
@@ -4,26 +4,26 @@ export default class CrudsController {
         const users = await User.all();
         return view.render('pages/admin/dashboard', { users });
     }
-    async create() {
+    async create({ response }) {
         const user = new User();
         const today = new Date();
         user.fullName = "Raihanssss";
         user.email = `${today.getTime()}@gmail.com`;
         user.password = 'hh';
-        user.save();
-        return Response.json(user);
+        await user.save();
+        return response.json(user);
     }
     async edit({ params, view }) {
         const user = await User.findOrFail(params.id);
         return view.render('pages/admin/edit', { user });
     }
-    async update({ request }) {
+    async update({ request, response }) {
         const user = await User.findOrFail(request.input('id'));
         user.fullName = request.input('fullname');
         user.email = request.input('email');
         user.password = 'hhd';
         await user.save();
-        return 'successfully updated' + request.input('id');
+        return response.redirect().toRoute('list');
     }
 }
 //# sourceMappingURL=crudsController.js.map
